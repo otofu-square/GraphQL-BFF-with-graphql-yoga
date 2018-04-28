476503f4973b3797508f26d9ba766d9530a8a978
otofu-square <yukita@feedforce.jp>
2018-04-28 13:26:56 +0900

    :wrench: Add fetch util functions

---
+++ src/utils/fetch.ts
@@ -0,0 +1,11 @@
+import fetch, { ResponseType } from 'node-fetch';
+
+type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+const HOST = 'https://jsonplaceholder.typicode.com';
+
+export const client = async <T>(method: Method, path: string) => {
+  const response = await fetch(`${HOST}${path}`, { method });
+  const json = await response.json<T>();
+  return json;
+};
